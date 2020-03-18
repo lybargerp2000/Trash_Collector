1ae1aa62acf665b5abd8f4d8d46bedd6145e2d38
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-03-18 14:39:19 -0500

    Had issue with json folder, Reverted to last commit.

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,15 +9,18 @@ namespace TrashCollector.Models
 {
     public class Customer
     {
-        public int CustomerId;
-        public string Name;
-        public string DayOfWeekChoice;
-        public double AccountBalance;
-        public string Adress;
-        public string Email;
-        public string Password;
-        public int ExtraDayDateChoice;
-        public int StartDate;
-        public int StopDate;
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string DayOfWeekChoice { get; set; }
+        public double AccountBalance { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int ExtraDayDateChoice { get; set; }
+        public int StartDate { get; set; }
+        public int StopDate { get; set; }
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }
