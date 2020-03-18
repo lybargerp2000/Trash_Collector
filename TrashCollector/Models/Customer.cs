4b848075b1643dbc8350aa53cc988df37494ca9f
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-03-18 13:45:33 -0500

    Setting up Customer Model first with required properties.

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        public int CustomerId;
+        public string Name;
+        public string DayOfWeekChoice;
+        public double AccountBalance;
+        public string Adress;
+        public string Email;
+        public string Password;
+        public int ExtraDayDateChoice;
+        public int StartDate;
+        public int StopDate;
+    }
+}
