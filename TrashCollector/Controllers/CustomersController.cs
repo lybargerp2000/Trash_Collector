14f75973381ca48fc59717a796db9a1ebc059ba4
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-03-18 16:53:26 -0500

    Setting up Roles.

--- TrashCollector/Controllers/CustomersController.cs
+++ TrashCollector/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -22,6 +23,7 @@ namespace TrashCollector.Controllers
         // GET: Customers
         public async Task<IActionResult> Index()
         {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
             var applicationDbContext = _context.Customer.Include(c => c.IdentityUser);
             return View(await applicationDbContext.ToListAsync());
         }
